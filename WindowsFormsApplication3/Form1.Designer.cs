c09c286f7ba2e0678e9a744f7ac81495b84f97db
Jaroslav Lauschmann <jjaartraa@gmail.com>
2021-01-20 23:41:10 +0100

    Přidejte soubory projektu.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CountB = new System.Windows.Forms.Button();
+            this.Copyright = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // CountB
+            // 
+            this.CountB.Location = new System.Drawing.Point(335, 519);
+            this.CountB.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.CountB.Name = "CountB";
+            this.CountB.Size = new System.Drawing.Size(80, 30);
+            this.CountB.TabIndex = 0;
+            this.CountB.Text = "Count";
+            this.CountB.UseVisualStyleBackColor = true;
+            this.CountB.Click += new System.EventHandler(this.CountB_Click);
+            // 
+            // Copyright
+            // 
+            this.Copyright.AutoSize = true;
+            this.Copyright.ForeColor = System.Drawing.SystemColors.Control;
+            this.Copyright.Location = new System.Drawing.Point(577, 539);
+            this.Copyright.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Copyright.Name = "Copyright";
+            this.Copyright.Size = new System.Drawing.Size(149, 13);
+            this.Copyright.TabIndex = 0;
+            this.Copyright.Text = "© 2018 Jaroslav Lauschmann";
+            this.Copyright.Click += new System.EventHandler(this.Copyright_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.LightBlue;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(710, 500);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(225, 516);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 30);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 547);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Loading...";
+            this.label1.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.DodgerBlue;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 526);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 20);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Support me";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Copyright);
+            this.Controls.Add(this.CountB);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "MessageCounter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CountB;
+        private System.Windows.Forms.Label Copyright;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Globalization;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String ConvName;
+        int ConvCount;
+        string[] directories;
+
+
+        private void CountB_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if (folderBrowserDialog1.SelectedPath != "")
+            {
+                try
+                {
+                    directories = Directory.GetDirectories(folderBrowserDialog1.SelectedPath + "\\messages\\inbox");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Invalid Directory");
+                }
+
+                CountB.Visible = false;
+                CountB.Enabled = false;
+                progressBar1.Visible = true;
+                progressBar1.Value = 0;
+                progressBar1.Maximum = directories.Count();
+                label1.Visible = true;
+                label1.BringToFront();
+
+                if (backgroundWorker1.IsBusy == false)
+                {
+                    backgroundWorker1.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void LocateMessagesInHTML(string path)
+        {
+            string[] messages = Directory.GetFiles(path,"message_*.html");
+            try
+            {
+                ConvName = null;
+                ConvCount = 0;
+
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.Load(path);
+                ConvName = doc.DocumentNode.SelectSingleNode("//head/title").InnerText;
+                byte[] bytes = Encoding.Default.GetBytes(ConvName);
+                ConvName = Encoding.UTF8.GetString(bytes);
+                this.Invoke((MethodInvoker)delegate
+                { label1.Text = "Loading:  " + ConvName; });
+                var CountNode = doc.DocumentNode.SelectNodes("//body/div/div/div");
+                FindNode(CountNode);
+                AddStats(ConvName, ConvCount);
+            }
+            catch (Exception) { }
+        }
+
+        private void Copyright_Click(object sender, EventArgs e)
+        {
+            if (Copyright.ForeColor == SystemColors.Control)
+                Copyright.ForeColor = SystemColors.ControlText;
+            else
+                Copyright.ForeColor = SystemColors.Control;
+        }
+
+        private void AddStats(string name = "Name not found", int count = 0)
+        {
+            string ForCount = count.ToString("### ###");
+
+            Button btn = new Button
+            {
+                Font = new Font("Arial Unicode MS", 8.25f),
+                Name = name,
+                Tag = count,
+                Text = name + Environment.NewLine + "Messages:" + Environment.NewLine + ForCount,
+                TextAlign = ContentAlignment.MiddleLeft,
+                Margin = new Padding(5, 5, 5, 5),
+                Height = 75,
+                Width = 165,
+                ForeColor = Color.Black,
+                BackColor = Color.White,
+                Padding = new Padding(15, 0, 0, 0)
+            };
+            this.Invoke((MethodInvoker)delegate
+            {
+                label1.Text = "Loading:  " + ConvName;
+                flowLayoutPanel1.Controls.Add(btn);
+                if (flowLayoutPanel1.VerticalScroll.Visible)
+                {
+                    flowLayoutPanel1.Width = 710 + 15;
+                    flowLayoutPanel1.Padding = new Padding(4, 0, 4, 0);
+                }
+                else
+                    flowLayoutPanel1.Width = 710;
+                var mySortedList = flowLayoutPanel1.Controls.OfType<Button>().OrderBy(x => -Convert.ToInt32(x.Tag)).ToList();
+                flowLayoutPanel1.Controls.AddRange(mySortedList.ToArray());
+                flowLayoutPanel1.Refresh();
+            });
+        }
+
+        private void FindNode(HtmlNodeCollection nodes)
+        {
+            foreach (var node in nodes)
+            {
+                try
+                {
+                    if (node.Attributes["role"].Value == "main")
+                        ConvCount = node.Elements("div").Count();
+                    break;
+                }
+                catch { }
+                if (node.HasChildNodes)
+                    FindNode(node.ChildNodes);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (var folder in directories)
+            {
+                LocateMessagesInHTML(folder + "\\message.html");
+                backgroundWorker1.ReportProgress(ConvCount);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = progressBar1.Value + 1;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Visible = false;
+            label1.Visible = false;
+            CountB.Visible = true;
+            CountB.Enabled = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://paypal.me/jjaartraa");
+        }
+    }
+}
